2eaeb74a47c6eac0204d0e14201ea9862bc9fe0a
Ananya Doshi <88143582+ananyadoshi31@users.noreply.github.com>
2024-08-25 22:54:44 +0000

    test cases added

---
+++ test_myfile.py
@@ -0,0 +1,11 @@
+from myfile import insertionSort
+
+def test_insertion_sort_pass():
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    assert arr == [5, 6, 11, 12, 13]
+
+def test_insertion_sort_fail():
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    assert arr == [12, 11, 13, 5, 6]
