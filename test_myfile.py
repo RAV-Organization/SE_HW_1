7b33b3acfd99e74ef70d0cb212e2dfe1e5e2e45c
Ananya Doshi <88143582+ananyadoshi31@users.noreply.github.com>
2024-08-25 23:45:44 +0000

    badges for failing

--- test_myfile.py
+++ test_myfile.py
@@ -5,7 +5,7 @@ def test_insertion_sort_pass():
     insertionSort(arr)
     assert arr == [5, 6, 11, 12, 13]
 
-# def test_insertion_sort_fail():
-#     arr = [12, 11, 13, 5, 6]
-#     insertionSort(arr)
-#     assert arr == [12, 11, 13, 5, 6]
+def test_insertion_sort_fail():
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    assert arr == [12, 11, 13, 5, 6]
